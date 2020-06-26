3a57306055ef09ff0d008c53a8105d1cc1f4bac4
Sam Rahimi <sam@sams-mbp.lan>
2020-06-26 12:43:40 -0400

    remove credential from github

--- src/start.ts
+++
@@ -1,7 +0,0 @@
-//WebHooks to receive events from Twitter
-//@mentions, DMs
-
-//Listen for bid activity on Arweave, save in DB, and retweet them
-
-//Listen for token transfer events (state changes in PST contracts), save in DB, and retweet *those*
- 
